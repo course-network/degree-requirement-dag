2687a06d6e2be3472b79fe9aca7ca8046a7717f2
Mintz <jmintz@contractor.usgs.gov>
2019-06-27 21:05:51 -0700

    Calculating number of dependencies by academic devision.

--- parse_course_requirements/igraph_v1.R
+++ parse_course_requirements/igraph_v1.R
@@ -5,12 +5,12 @@ library(igraph)
 
 reqs <- read.csv("./parse_course_requirements/edges.csv", header = F)
 head(reqs)
-names(reqs) <- c("course", "pre_rec")
+names(reqs) <- c("pre_rec", "course")
 
 
 req_net <- graph_from_data_frame(reqs, directed = T)
 plot(req_net, directed = T)
-tree_layout <- layout.reingold.tilford(req_net, root = "ST623", circular = FALSE, flip.y = FALSE)
+tree_layout <- layout.reingold.tilford(req_net, root = "ST 623", circular = FALSE, flip.y = FALSE)
 
 
 # stat <- stat %>% select(ref, course, pre_rec, name)
@@ -26,3 +26,38 @@ plot(st_net, edge.color = stat$pre_rec, edge.curved = 0.3, edge.width = 2) # Thi
 # Calculate node degree
 degree(st_net)
 plot(st_net, vertex.color = degree(st_net))
+
+
+
+
+
+# Counting dependencies ------------------------------
+
+# Pre requisite department (academic unit)
+reqs$pre_dept <- sub(x = reqs$pre_rec, 
+                     pattern = "([A-z]+) .+", 
+                     replacement = "\\1")
+
+# Course department (academic unit)
+reqs$course_dept <- sub(x = reqs$course, 
+                     pattern = "([A-z]+) .+", 
+                     replacement = "\\1")
+
+# There are about half of pre-requisites that
+#  were in-department.
+table(reqs$pre_dept == reqs$course_dept)
+
+reqs$pre_dept[reqs$pre_dept == "NE"] <- "NSE"
+# Count number of dependencies.
+reqs %>% filter(pre_dept != "of") %>%
+  group_by(pre_dept) %>% 
+  summarise(n = n()) %>% arrange(desc(n))
+
+# Count number of 
+# Nuclear science got changed to NSE.
+reqs$pre_dept[reqs$pre_dept == "NE"] <- "NSE"
+reqs %>% filter(pre_dept != "and", pre_dept != "or", pre_dept != "of", pre_dept != course_dept) %>%
+  group_by(pre_dept) %>% 
+  summarise(n = n()) %>% arrange(desc(n))
+
+
