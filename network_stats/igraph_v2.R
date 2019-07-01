695abb8c913c4d784daed1a9916784f54af729b5
Mintz <jmintz@contractor.usgs.gov>
2019-06-30 20:49:51 -0700

    Coloring OR nodes with igraph.

--- network_stats/igraph_v2.R
+++ network_stats/igraph_v2.R
@@ -3,8 +3,8 @@ library(dplyr)
 library(igraph)
 
 
-reqs <- read.csv("./parse_course_requirements/edges.csv", header = F)
-# reqs <- read.csv("../Pickle to CSV/6-30/edges.csv", header = F)
+# reqs <- read.csv("./parse_course_requirements/edges.csv", header = F)
+reqs <- read.csv("./pickle_to_csv/edges.csv", header = F)
 #  Using this file locally until we merge branches.
 
 
@@ -72,9 +72,31 @@ reqs %>% filter(pre_dept != "and", pre_dept != "or", pre_dept != "of", pre_dept 
 
 # Create a small plot, math meets CS ------------------------------
 # This will require the OR node data.
+reqs$pre_dept <- sub(x = reqs$pre_rec, 
+                     pattern = "([A-z]+) .+", 
+                     replacement = "\\1")
+
+# Course department (academic unit)
+reqs$course_dept <- sub(x = reqs$course, 
+                        pattern = "([A-z]+) .+", 
+                        replacement = "\\1")
+
+sub_reqs <- reqs %>% filter(pre_dept %in% c("MTH", "CS") | course_dept %in% c("MTH", "CS")) %>% head(100)
+
+sub_net <- graph_from_data_frame(sub_reqs, directed = T)
 
 
+sub_reqs$or <- 1 + 1*(sub_reqs$course_dept == "OR")
+sub_reqs$or_color <- c("grey", "steelblue")[sub_reqs$or]
+plot(sub_net, directed = T, edge.color = sub_reqs$or, vertex.size = 2)
 
+# Full graph with character expand smaller.
+reqs$or <- 1 + 1*(reqs$course_dept == "OR")
+full_net <- graph_from_data_frame(reqs, directed = T)
+plot(sub_net, directed = T, edge.color = reqs$or, vertex.size = 2, arrow.width = 0.5, label.cex = 5)
 
-
-
+V(sub_net)$label.cex = 0.65
+V(sub_net)$arrow.size = 0.2
+E(sub_net)$edge.color = sub_reqs$or_color
+V(sub_net)$vertex.size = 1
+plot.igraph(sub_net)
