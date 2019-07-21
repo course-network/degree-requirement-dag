8817e03672a1bb8d970698c599518f8196150afe
Mintz <jmintz@contractor.usgs.gov>
2019-07-21 12:55:58 -0700

    Layouts with symbols for OR nodes in igraph.

--- network_stats/igraph_v2.R
+++ network_stats/igraph_v2.R
@@ -70,6 +70,18 @@ reqs %>% filter(pre_dept != "and", pre_dept != "or", pre_dept != "of", pre_dept 
 #  Count it proportional to the number of elements in the OR maybe?
 
 
+# reqs$pre_dept[reqs$pre_dept == "NE"] <- "NSE"
+# External pre-requisites
+reqs %>% filter(pre_dept != course_dept) %>%
+  group_by(pre_dept) %>% filter(pre_dept != "OR") %>%
+  summarise(n = n()) %>% arrange(desc(n))
+
+reqs %>% 
+  group_by(pre_dept) %>% filter(pre_dept != "OR") %>%
+  summarise(n = n()) %>% arrange(desc(n))
+
+
+
 # Create a small plot, math meets CS ------------------------------
 # This will require the OR node data.
 reqs$pre_dept <- sub(x = reqs$pre_rec, 
@@ -100,3 +112,112 @@ V(sub_net)$arrow.size = 0.2
 E(sub_net)$edge.color = sub_reqs$or_color
 V(sub_net)$vertex.size = 1
 plot.igraph(sub_net)
+
+
+
+
+
+# The most popular course ------------------------------
+head(reqs)
+
+# This will require the OR node data.
+reqs$pre_dept <- sub(x = reqs$pre_rec, 
+                     pattern = "([A-z]+) .+", 
+                     replacement = "\\1")
+
+# Course department (academic unit)
+reqs$course_dept <- sub(x = reqs$course, 
+                        pattern = "([A-z]+) .+", 
+                        replacement = "\\1")
+
+# Some OR nodes have a lot of options.
+top10 <- reqs %>% filter(course_dept == "OR") %>% 
+  group_by(course) %>% summarise(n_pr = n()) %>% arrange(desc(n_pr)) %>% head(10)
+
+library(pander)
+pander(left_join(top10, reqs, by = c("course" = "pre_rec")))
+
+
+reqs %>% filter(course_dept != "OR") %>% 
+  group_by(course) %>% summarise(n_pr = n()) %>% arrange(desc(n_pr)) %>% head(10)
+
+
+
+
+sub_reqs <- reqs %>% filter(pre_dept %in% c("MTH", "CS") | course_dept %in% c("MTH", "CS")) %>% head(100)
+
+
+
+
+# Distribution of number of pre-requisites:
+# X courses have no pre-requisite.
+
+# ALS courses ------------------------------
+# Color, size, and shape working out.
+
+reqs <- read.csv("./pickle_to_csv/edges.csv", header = F)
+
+names(reqs) <- c("pre_rec", "course")
+
+# This will require the OR node data.
+reqs$pre_dept <- sub(x = reqs$pre_rec, 
+                     pattern = "([A-z]+) .+", 
+                     replacement = "\\1")
+
+# Course department (academic unit)
+reqs$course_dept <- sub(x = reqs$course, 
+                        pattern = "([A-z]+) .+", 
+                        replacement = "\\1")
+
+head(reqs)
+dim(reqs)
+
+# sub_reqs <- reqs %>% filter(pre_dept %in% c("MTH", "CS") | course_dept %in% c("MTH", "CS")) %>% head(100)
+sub_reqs <- reqs %>% head(6)
+
+courses <- c("ALS 151", "ALS 161", "ALS 162", "OR 0", "OR 1")
+sub_reqs <- reqs %>% filter(pre_rec %in% courses | course %in% courses)
+sub_reqs$or <- 1 + 1*(sub_reqs$course_dept == "OR")
+
+sub_net <- graph_from_data_frame(sub_reqs, directed = T)
+plot.igraph(sub_net)
+plot(sub_net, directed = T, 
+     edge.color = sub_reqs$or, 
+     vertex.size = 12, 
+     vertex.shape = c("circle", "square")[grepl(x = names(V(sub_net)), pattern = "OR.+") + 1], 
+     vertex.color =  c("grey", "steelblue")[grepl(x = names(V(sub_net)), pattern = "OR.+") + 1])
+
+
+
+# Might try with certain CS courses?
+
+# sub_reqs <- reqs %>% filter(pre_dept %in% c("MTH", "CS") | course_dept %in% c("MTH", "CS"))
+
+# Math oR CS
+# sub_reqs <- reqs %>% filter(course_dept %in% c("MTH", "CS"))
+head(reqs)
+csor <- reqs$pre_rec[(reqs$pre_dept == "OR" & reqs$course_dept == "CS")]
+reqs$csor <- reqs$course %in% csor
+sum(reqs$csor)
+
+
+sub_reqs <- reqs %>% filter(course_dept %in% c("CS") | csor)
+sub_reqs$or <- 1 + 1*(sub_reqs$course_dept == "OR")
+
+sub_net <- graph_from_data_frame(sub_reqs, directed = T)
+plot.igraph(sub_net)
+plot(sub_net, directed = T, 
+     edge.color = sub_reqs$or, 
+     vertex.size = 12, 
+     vertex.shape = c("circle", "square")[grepl(x = names(V(sub_net)), pattern = "OR.+") + 1], 
+     vertex.color =  c("grey", "steelblue")[grepl(x = names(V(sub_net)), pattern = "OR.+") + 1])
+
+
+# Let's spread the points better
+fr_net <- layout_nicely(sub_net)
+plot(sub_net, layout = fr_net, directed = T, 
+     edge.color = sub_reqs$or, 
+     vertex.size = 5, 
+     vertex.shape = c("circle", "square")[grepl(x = names(V(sub_net)), pattern = "OR.+") + 1], 
+     vertex.color =  c("grey", "steelblue")[grepl(x = names(V(sub_net)), pattern = "OR.+") + 1])
+
