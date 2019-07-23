eddd326bdb9f5d138521465324e586297135ff5d
wilsonia <wilsonia@oregonstate.edu>
2019-07-22 23:56:49 -0700

    parse course requirements v2

--- parse_course_requirements/parse_course_requirements.py
+++ parse_course_requirements/parse_course_requirements.py
@@ -9,40 +9,96 @@ courses_parsed = {}
 or_id = 0
 
 
-def parse_prereqs(parent, prereqs):
+def apply_regex(prereqs, indices, output):
+    if isinstance(prereqs, list):
+        for idx, prereq in enumerate(prereqs):
+            # True if trailing or
+            if apply_regex(prereq, indices + [idx], output):
+                prereqs[idx + 1].insert(0, 'OR')
+    else:
+        regex_match = re.findall(r'[^[( ]+ ?[^[( a-z]+ [\dH]+', prereqs)
+        if regex_match:
+            output[tuple(indices)] = regex_match
+        # Handle trailing or
+        if prereqs is 'OR':
+            output[tuple(indices)] = prereqs
+        return prereqs.endswith('or ')
+
+
+def update_node(course, prereqs):
+    existing_prereqs = [] if course not in courses_parsed else courses_parsed[
+      course]['prereqs']
     for prereq in prereqs:
-        if isinstance(prereq, list):
-            if any(isinstance(p, str) and len(re.findall(r'[^[( a-z]+ [\dH]+',
-                                              p)) > 0 for p in prereq):
-                global or_id
-                parse_prereqs(f'OR {or_id}', prereq)
-                prereqs.append(f'OR {or_id}')
-                or_id += 1
+        if not existing_prereqs or prereq not in existing_prereqs:
+            existing_prereqs.append(prereq)
+    # if OR present, it is an OR-1 node, default is AND
+    or_magnitude = len(existing_prereqs)
+    if any(i is 'OR' for i in existing_prereqs):
+        or_magnitude = 1
+    courses_parsed[course] = {'prereqs': existing_prereqs,
+                              'or_magnitude': or_magnitude}
+
+
+def generate_course_object(course, regexed_prereqs):
+    global or_id
+    # Loop through top level
+    for key in set([i[0] for i in regexed_prereqs]):
+        # Get all keys in belonging to top level iteration
+        sub_keys = [i for i in regexed_prereqs if i[0] == key]
+        # If there are multiple subkeys, create or node and make recursive call
+        if len(sub_keys) > 1:
+            or_id += 1
+            or_prereqs = {}
+            for idx, sub_key in enumerate(sub_keys):
+                or_prereqs[sub_key[1:]] = regexed_prereqs[sub_key]
+            update_node(course, [f'OR {or_id}'])
+            generate_course_object(f'OR {or_id}', or_prereqs)
+        # If only one subkey, add prereq to parsed_courses
         else:
-            prereq = re.findall(r'[^[( a-z]+ [\dH]+', prereq)
-            or_magnitude = 1 if parent.startswith('OR ') else 0
-            if parent in courses_parsed:
-                prereq.extend(courses_parsed[parent]['prereqs'])
-            courses_parsed[parent] = {
-                'or_magnitude': or_magnitude,
-                'prereqs': prereq
-            }
+            if len(sub_keys[0]) == 1:
+                # Handle trailing OR
+                if any(i is 'O' for i in regexed_prereqs[sub_keys[0]]):
+                    for key in courses_parsed:
+                        if course in courses_parsed[key]['prereqs']:
+                            update_node(key, ['OR'])
+                update_node(course, [i for i in regexed_prereqs[
+                  sub_keys[0]] if i != 'O' and i != 'R'])
+            # reformat list of strings as nested object to generate OR-1 node
+            else:
+                prereqs = {}
+                prereqs[(0, 0)] = ['OR']
+                for idx, prereq in enumerate(regexed_prereqs[sub_keys[0]]):
+                    prereqs[(0, idx + 1)] = [prereq]
+                generate_course_object(course, prereqs)
+
+
+def clean_parsed_courses(courses):
+    for key in courses:
+        courses[key]['prereqs'] = re.findall(r'[^[( a-z]+ [\dH]+',
+                                             ' '.join(courses[key]['prereqs']))
 
 
 for course, prereq_str in courses.items():
+    # Fix errors in catalog
     if course in ['HORT 360', 'NSE 311']:
-        prereq_str = prereq_str + ')'
+            prereq_str = prereq_str + ')'
+
+    # Create nested lists delimited by parantheses
+    parser = pyparsing.nestedExpr(content=pyparsing.CharsNotIn('()'))
+    nested_prereqs = parser.parseString(f'({prereq_str})').asList()[0]
+
+    # Traverse list, converting strings to regex matches
+    nested_prereqs_regexed = {}
+    apply_regex(nested_prereqs, [], nested_prereqs_regexed)
+
+    # Traverse regexed object, adding OR nodes
+    generate_course_object(course, nested_prereqs_regexed)
 
     print(course)
 
-    parser = pyparsing.nestedExpr(content=pyparsing.CharsNotIn('()'))
-    nested_prereqs = parser.parseString(f'({prereq_str})').asList()[0]
-    for prereqs in nested_prereqs:
-        if isinstance(prereqs, str):
-            prereq_count = len(re.findall(r'[^[( a-z]+ [\dH]+', prereqs))
-            if prereq_count > 1 and 'and' not in prereqs:
-                prereqs = [f'({prereqs})']
-        parse_prereqs(course, [prereqs])
+# Clean courses_parsed for OR, and, or by matching course number regex
+clean_parsed_courses(courses_parsed)
 
-courses_file = open('./courses_parsed.obj', 'wb')
-courses = pickle.dump(courses_parsed, courses_file)
+# Save parsed courses
+courses_parsed_file = open('courses_parsed.obj', 'wb')
+pickle.dump(courses_parsed, courses_parsed_file)
