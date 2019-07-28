9d6d08399be101789e59556e33f25819dbc06dc2
ianbrown9475 <ian.brown9475@gmail.com>
2019-07-28 00:20:35 -0700

    Combine get degree requirements logic

---
+++ degree_requirements/config.py
@@ -0,0 +1,4 @@
+# URL to course requirements page
+url = 'https://catalog.oregonstate.edu/college-departments/engineering/school-electrical-engineering-computer-science/computer-science-ba-bs-hba-hbs/computer-systems-option/#requirementstext'
+# Path to output file
+output_path = './mock_data_filtered.json'
--- degree_requirements/get_degree_requirements.py
+++ degree_requirements/get_degree_requirements.py
@@ -1,11 +1,21 @@
 import json
+import unicodedata
+
+from bs4 import BeautifulSoup
+import requests
+
+from config import url, output_path
+
+html = requests.get(url).text
+soup = BeautifulSoup(html, 'html.parser')
+courses = list(map(
+    lambda x: unicodedata.normalize('NFKD', x['title']),
+    soup.find_all(class_='bubblelink code')
+))
 
 obj = json.load(open('../mock_data.json'))
-nodes = list(filter(lambda x: 'CS ' in x['id'], obj['nodes']))
-classes = list(map(lambda x: x['id'], nodes))
-
 links = list(filter(
-    lambda x: x['target'] in classes,
+    lambda x: x['target'] in courses,
     obj['links']
 ))
 
@@ -23,9 +33,9 @@ while True:
 
 new_sources = set(map(lambda x: x['source'], links))
 new_targets = set(map(lambda x: x['target'], links))
-new_classes = list(new_sources.union(new_targets))
+new_courses = list(new_sources.union(new_targets))
 
-nodes = list(filter(lambda x: x['id'] in new_classes, obj['nodes']))
+nodes = list(filter(lambda x: x['id'] in new_courses, obj['nodes']))
 
 # Sort nodes by 'id' and links by 'source' and 'target'
 nodes.sort(key=lambda x: x['id'])
@@ -33,11 +43,9 @@ links.sort(key=lambda x: f"{x['source']}|{x['target']}")
 
 filtered = {'links': links, 'nodes': nodes}
 
-output_path = './mock_data_filtered.json'
+# output_path = './mock_data_filtered.json'
 with open(output_path, 'w') as f:
     f.write(json.dumps(filtered, sort_keys=True, indent=4))
 
-print(
-    f"result: {len(filtered['nodes'])} nodes, {len(filtered['links'])} edges"
-)
+print(f"result: {len(nodes)} nodes, {len(links)} edges")
 print(f'wrote file to {output_path}')
