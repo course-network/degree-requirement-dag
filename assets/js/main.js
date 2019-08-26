4b3955946f828941206f8cc76dd4d592039c2545
wilsonia <wilsonia@oregonstate.edu>
2019-08-25 22:51:33 -0700

    CRN as y-axis

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ var simulation = d3.forceSimulation()
                    .force("attractForce", attractForce)
                    .force("repelForce", repelForce);
 
-d3.json('course_data/physics_courses.json', function(error, graph) {
+d3.json('course_data/d3/physics_courses.json', function(error, graph) {
   if (error) throw error;
 
   var link = svg.append('g')
--- d3/index.js
+++ d3/index.js
@@ -19,12 +19,16 @@ var colors = d3.scaleOrdinal(d3.schemeCategory10);
         .attr('fill', '#999')
         .style('stroke','none');
 
+    var attractForce = d3.forceManyBody().strength(100).distanceMax(400).distanceMin(60);
+    var repelForce = d3.forceManyBody().strength(-400).distanceMax(300).distanceMin(0);
     var simulation = d3.forceSimulation()
         .force("link", d3.forceLink().id(function (d) {return d.id;}).distance(100).strength(1))
         .force("charge", d3.forceManyBody())
-        .force("center", d3.forceCenter(width / 2, height / 2));
+        .force("center", d3.forceCenter(width / 2, height / 2))
+        .force("attractForce", attractForce)
+        .force("repelForce", repelForce);
 
-    d3.json("./graph.json", function (error, graph) {
+    d3.json("../course_data/d3/physics_courses.json", function (error, graph) {
         if (error) throw error;
         update(graph.links, graph.nodes);
     })
@@ -37,9 +41,6 @@ var colors = d3.scaleOrdinal(d3.schemeCategory10);
             .attr("class", "link")
             .attr('marker-end','url(#arrowhead)')
 
-        link.append("title")
-            .text(function (d) {return d.type;});
-
         edgepaths = svg.selectAll(".edgepath")
             .data(links)
             .enter()
@@ -51,25 +52,6 @@ var colors = d3.scaleOrdinal(d3.schemeCategory10);
                 'id': function (d, i) {return 'edgepath' + i}
             })
             .style("pointer-events", "none");
-
-        edgelabels = svg.selectAll(".edgelabel")
-            .data(links)
-            .enter()
-            .append('text')
-            .style("pointer-events", "none")
-            .attrs({
-                'class': 'edgelabel',
-                'id': function (d, i) {return 'edgelabel' + i},
-                'font-size': 10,
-                'fill': '#aaa'
-            });
-
-        edgelabels.append('textPath')
-            .attr('xlink:href', function (d, i) {return '#edgepath' + i})
-            .style("text-anchor", "middle")
-            .style("pointer-events", "none")
-            .attr("startOffset", "50%")
-            .text(function (d) {return d.type});
 
         node = svg.selectAll(".node")
             .data(nodes)
@@ -91,7 +73,7 @@ var colors = d3.scaleOrdinal(d3.schemeCategory10);
 
         node.append("text")
             .attr("dy", -3)
-            .text(function (d) {return d.name+":"+d.label;});
+            .text(function (d) {return d.id+":";});
 
         simulation
             .nodes(nodes)
@@ -109,33 +91,16 @@ var colors = d3.scaleOrdinal(d3.schemeCategory10);
             .attr("y2", function (d) {return d.target.y;});
 
         node
-            .attr("transform", function (d) {return "translate(" + d.x + ", " + d.y + ")";});
-
+            .attr("transform", function (d) {return "translate(" + d.x + ", " + d.fy + ")";});
         edgepaths.attr('d', function (d) {
             return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y;
-        });
-
-        edgelabels.attr('transform', function (d) {
-            if (d.target.x < d.source.x) {
-                var bbox = this.getBBox();
-
-                rx = bbox.x + bbox.width / 2;
-                ry = bbox.y + bbox.height / 2;
-                return 'rotate(180 ' + rx + ' ' + ry + ')';
-            }
-            else {
-                return 'rotate(0)';
-            }
         });
     }
 
     function dragstarted(d) {
-        if (!d3.event.active) simulation.alphaTarget(0.3).restart()
-        d.fx = d.x;
-        d.fy = d.y;
+        return null;
     }
 
     function dragged(d) {
-        d.fx = d3.event.x;
-        d.fy = d3.event.y;
+        return null;
     }
