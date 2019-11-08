de7e2a8395c0e61824caab546f9a5a9a199685b3
Mintz <jmintz@contractor.usgs.gov>
2019-11-07 16:48:09 -0800

    Split CS data exploration file into data handling CS file that creates a csv and visualization file for plot.

---
+++ network_stats/cs_subset.R
@@ -0,0 +1,55 @@
+# cs_subset.R ------------------------------
+#' 
+#' Create a subset of courses to work on for CS.
+#'  Includes all courses in CS, plus all courses and OR nodes which
+#'  are pre-requisites for CS.
+#'
+# ....................................
+
+
+
+library(dplyr)
+library(igraph)
+
+
+reqs <- read.csv("./course_data/csv/courses_parsed_edges.csv", header = F)
+node <- read.csv("./course_data/csv/courses_parsed_nodes.csv", header = F)
+
+names(reqs) <- c("pre_req", "course")
+names(node) <- c("course", "order")
+
+# Identify Academic Units.
+# Pre requisite department (academic unit)
+reqs$pre_dept <- sub(x = reqs$pre_req,
+                     pattern = "([A-z]+) .+",
+                     replacement = "\\1")
+
+# Course department (academic unit)
+reqs$course_dept <- sub(x = reqs$course,
+                        pattern = "([A-z]+) .+",
+                        replacement = "\\1")
+
+# reqs$pre_dept[reqs$pre_dept == "NE"] <- "NSE"
+reqs <- reqs %>% filter(pre_dept != "NE")
+
+
+
+# Try to get CS only.
+head(reqs)
+head(node)
+
+# 20 items in CS requires something as an OR. 
+or_to_cs <- reqs %>% filter(course_dept == "CS" & pre_dept == "OR")
+# 27 courses lead into a CS OR.
+input_to_cs_or <- reqs %>% filter(course %in% or_to_cs$pre_req)
+# 16 courses lead into CS which are not an OR.
+input_to_cs <- reqs %>% filter(course_dept == "CS" & !(pre_dept %in% c("OR", "CS")))
+# 68 courses lead from CS to CS
+cs_to_cs <- reqs %>% filter(course_dept == "CS" & pre_dept == "CS")
+# Combine and filter
+cs <- rbind(or_to_cs, 
+            input_to_cs_or,
+            input_to_cs,
+            cs_to_cs) %>% distinct()
+
+head(cs)
